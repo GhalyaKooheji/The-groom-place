02d7fcd9f45c65ea042d7bbcd15f3caebb198811
GhalyaKooheji <202401762@student.polytechnic.bh>
2025-10-19 17:27:32 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,2 @@
+document.getElementByid("book appointment").onsubmit=function();{alert("Appointment booked"); return false;};
+document.getElementByTagName("tr")[1].onclick=function();{alert("Hair cut foe 4BD");};
